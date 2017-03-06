85892b6e5ae5f61da34cf8ffca301ffa3db4c74c
Siddharth Trehan <trehans@mit.edu>
2017-03-05 23:24:57 -0500

    Got DC motors, frame servo, and latch servo to work and respond to UART

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -4,61 +4,74 @@
 volatile uint8_t* timers[] = {0x00, 0x00, &TCCR3B, &TCCR3B, &TCCR0B, &TCCR3B, &TCCR4B, &TCCR4B, &TCCR4B, &TCCR2B, &TCCR2B, &TCCR1B, &TCCR1B, &TCCR0B};
 
 // DC motor configuration
-int motorPin1 = 2;
-int motorPin2 = 3;
+int motorPin = 2;
 byte speed = 0x40;
 
 // Servo motor configuration
-Servo servo1;
-Servo servo2;
-byte servoPin1 = 9;
-byte servoPin2 = 10;
-byte servoPos1 = 0;
-byte servoPos2 = 0;
+Servo servoFrame;
+Servo servoLatch;
+byte servoFramePin = 9;
+byte servoLatchPin = 10;
 
 void setup() {
   // Set the correct PWM frequency on the two motor pins
-  *timers[motorPin1] = (*timers[motorPin1] & 0xF8) | 0x01;
-  *timers[motorPin1] = (*timers[motorPin1] & 0xF8) | 0x01;
+  *timers[motorPin] = (*timers[motorPin] & 0xF8) | 0x01;
 
   // Run the motor pins at their initial speed
-  analogWrite(motorPin1, ~speed);
-  analogWrite(motorPin2, ~speed);
+  analogWrite(motorPin, ~speed);
 
   // Begin serial communication over USB CDC on pins 0 and 1
   Serial.begin(9600);
 
   // Set up the servos
-  servo1.attach(servoPin1);
-  servo2.attach(servoPin2);
+  servoFrame.attach(servoFramePin);
+  servoLatch.attach(servoLatchPin);
 }
 
 void loop() {
   // Change the speed of the DC motor if a command arrives over serial port
   if (Serial.available()) {
     char command = Serial.read();
-    if (command == 'S') {
-      if (Serial.available()) {
-        // Next byte should be hex code indicating speed (between 0 and F)
-        char speedInput = Serial.read();
-        speed = hexChar(speedInput) << 3;
+    if (command == 'L') {
+      // L: Set the speed, move the frame to the right angle, and open the latch
+      
+      // Wait until another byte arrives
+      while (!Serial.available()) {}
+      
+      // Next byte should be hex code indicating speed (between 0 and F)
+      char speedInput = Serial.read();
+      speed = hexChar(speedInput) << 4 | 0x0F;
 
-        // Run the DC motors at this speed
-        analogWrite(motorPin1, ~speed);
-        analogWrite(motorPin2, ~speed);
-      }
+      // Run the DC motors at this speed
+      analogWrite(motorPin, ~speed);
+
+      // Wait until another byte arrives
+      while (!Serial.available()) {}
+
+      // Next byte should be hex code indicating high nibble of angle (between 0 and F)
+      byte angle = 0x00;
+      char highByteInput = Serial.read();
+      angle = hexChar(highByteInput) << 4;
+
+      // Wait until another byte arrives
+      while (!Serial.available()) {}
+
+      // Next byte should be hex code indicating low nibble of angle (between 0 and F)
+      char lowByteInput = Serial.read();
+      angle = angle | hexChar(lowByteInput);
+
+      // Move the servo to the angle
+      servoFrame.write(angle);
+
+      // Delay a little bit and open the latch
+      delay(1000);
+      servoLatch.write(135);
+      delay(2000);
+      servoLatch.write(0);
     } else {
-      // Do nothing
+      Serial.write('x');
     }
   }
-  
-  // in steps of 1 degree
-  /*servo1.write(pos);              // tell servo to go to position in variable 'pos'
-  pos = (pos + 1) % 180;
-  delay(15);                       // waits 15ms for the servo to reach the position
-  if (pos == 0x00) {
-    delay(1000);
-  }*/
 }
 
 byte hexChar(char s) {
